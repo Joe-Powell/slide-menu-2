b161e6b7ffe1161c4951d0f5cf7cedb9211de34a
Joe Powell <joe_773@yahoo.com>
2021-07-06 10:43:41 -0500

    change

--- main.js
+++ main.js
@@ -5,3 +5,25 @@ const nav2 = document.querySelector('.nav2');
 hamburger.addEventListener('click', () => {
     nav2.classList.toggle('onclick');
 })
+
+
+
+// sub menu in .nav2
+
+const subTrigger = document.querySelector('.subTrigger')
+const subMenu = document.querySelector('.nav2 .sub-menu')
+const arrow = document.querySelector('.fa-arrow-right');
+
+const activeSubMenu = document.querySelector('.activeSubMenu')
+
+subTrigger.addEventListener('click', () => {
+    console.log('clicked has sub')
+
+    subMenu.classList.toggle('activeSubMenu')
+    arrow.classList.toggle('rotateArrow')
+
+
+})
+
+
+
